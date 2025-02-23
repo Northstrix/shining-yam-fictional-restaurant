328fae32a81d0c640cb91a71af024990e1599948
Maxim Bortnikov | מקסים בורטניקוב <43994704+Northstrix@users.noreply.github.com>
2025-02-23 20:51:18 +0700

    Add files via upload

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,18 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable or adjust specific ESLint rules
+      "react/no-unescaped-entities": "off", // Allow unescaped entities like single quotes
+      "@typescript-eslint/no-unused-expressions": "off", // Disable unused expressions rule
+      "@typescript-eslint/no-unused-vars": "off", // Disable unused variables rule
+      "@typescript-eslint/no-explicit-any": "off", // Allow usage of `any`
+      "react-hooks/exhaustive-deps": "warn", // Warn instead of error for missing dependencies in useEffect
+      "@next/next/no-img-element": "off", // Allow usage of <img> instead of <Image />
+      "prefer-const": "off", // Disable prefer-const rule
+    },
+  },
 ];
 
 export default eslintConfig;
